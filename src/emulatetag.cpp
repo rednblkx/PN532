4bea3b1974fbb7125ae1b7c649eb3915d6457dcd
White Red <rednblkx@protonmail.com>
2024-03-06 01:26:12 +0200

    fix compiler error-warning

--- src/emulatetag.cpp
+++ src/emulatetag.cpp
@@ -129,8 +129,8 @@ bool EmulateTag::emulate(const uint16_t tgInitAsTargetTimeout)
   tagWrittenByInitiator = false;
 
   uint8_t rwbuf[128];
-  uint8_t sendlen;
-  int16_t status;
+  uint8_t sendlen = 0;
+  int16_t status = -1;
   tag_file currentFile = NONE;
   uint16_t cc_size = sizeof(compatibility_container);
   bool runLoop = true;
